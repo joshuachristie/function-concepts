c65df7b4a0e2f49799aa1f0c4c6671e6b997c01f
Joshua Christie <joshua.christie@sydney.edu.au>
2020-07-14 19:17:31 +1000

    initial commit of files to build Docker image for handbook testing
    used to create joshuarchristie/jupyter-spacy:first_attempt

---
+++ docker/test/jupyter_spacy/process_sentence.py
@@ -0,0 +1,28 @@
+import spacy
+from spacy import displacy
+
+def print_info(doc, tok_width=20, dep_rel_width=30, head_width=20, dependencies_width=40):
+    """
+    prints information about token and head dependencies
+    args:
+        doc: spacy doc
+        tok_width: width of col 1
+        dep_rel_width: width of col 2
+        head_width: width of col 3
+        dependencies_width: width of col 4
+    returns:
+        Nothing (prints output to screen)
+    """
+    print("{:<{}}{:<{}}{:<{}}{:<{}}\n".format("TOKEN", tok_width, "DEPENDENCY RELATION", dep_rel_width,
+                                              "HEAD", head_width, "DEPENDENCIES", dependencies_width))
+    for token in doc:
+        print("{:<{}}{:<{}}{:<{}}{}".format(
+            token.text, tok_width, str(spacy.explain(token.dep_)), dep_rel_width,
+            token.head.text, head_width, [child for child in token.children]))
+
+def show_dependency_graph(doc, width):
+    displacy.render(doc, style="dep", jupyter=True, options={'distance' : width})
+
+def visualise(doc, width=100):
+    print_info(doc)
+    show_dependency_graph(doc, width)
